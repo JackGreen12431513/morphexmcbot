a0b91c1c0566242be2236342262c49de502229ee
unknown <preazy.rblx@gmail.com>
2018-08-17 18:17:07 +0100

    Commit title

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var client = new discord.Client();
 const fs = require('fs')
 var request = require('request');
 const repeat = require('repeat');
+const ytdl = require("ytdl-core");
+const request = require("request");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
 require('dotenv').config();
 const conf = require('./config.json', 'utf8');
 console.log(token);
@@ -13,6 +17,10 @@ var token = conf.botToken;
 
 var usersPlaying = [];
 var noMentionArray = [];
+
+
+var guilds = {};
+
 
 
 function checkIfOnline(){
@@ -94,6 +102,61 @@ client.on('message', message => {
         else
             return text;
       }
+
+    if (!guilds[message.guild.id]) {
+        guilds[message.guild.id] = {
+            queue: [],
+            queueNames: [],
+            isPlaying: false,
+            dispatcher: null,
+            voiceChannel: null,
+            skipReq: 0,
+            skippers: []
+        };
+    }
+
+      if (message.startsWith(prefix + "play")) {
+        if (message.member.voiceChannel || guilds[message.guild.id].voiceChannel != null) {
+            if (guilds[message.guild.id].queue.length > 0 || guilds[message.guild.id].isPlaying) {
+                getID(args, function(id) {
+                    add_to_queue(id, message);
+                    fetchVideoInfo(id, function(err, videoInfo) {
+                        if (err) throw new Error(err);
+                        message.reply(" added to queue: **" + videoInfo.title + "**");
+                        guilds[message.guild.id].queueNames.push(videoInfo.title);
+                    });
+                });
+            } else {
+                isPlaying = true;
+                getID(args, function(id) {
+                    guilds[message.guild.id].queue.push(id);
+                    playMusic(id, message);
+                    fetchVideoInfo(id, function(err, videoInfo) {
+                        if (err) throw new Error(err);
+                        guilds[message.guild.id].queueNames.push(videoInfo.title);
+                        message.reply(" now playing: **" + videoInfo.title + "**");
+                    });
+                });
+            }
+        } else {
+            message.reply(" you need to be in a voice channel!");
+        }
+    }
+        else if (message.startsWith(prefix + "queue")) {
+            var message2 = "```";
+            for (var i = 0; i < guilds[message.guild.id].queueNames.length; i++) {
+                var temp = (i + 1) + ": " + guilds[message.guild.id].queueNames[i] + (i === 0 ? "**(Current Song)**" : "") + "\n";
+                if ((message2 + temp).length <= 2000 - 3) {
+                    message2 += temp;
+                } else {
+                    message2 += "```";
+                    message.channel.send(message2);
+                    message2 = "```";
+                }
+            }
+            message2 += "```";
+            message.channel.send(message2);
+        }
 
     switch(args[0]) {
 
@@ -619,6 +682,71 @@ function addRole(id, role) {
     id.addRole(role).catch(console.error);
 }
 
+client.login(token);
+
+function playMusic(id, message) {
+    userVC = message.member.voiceChannel;
+
+
+
+    userVC.join().then(function(connection) {
+        stream = ytdl("https://www.youtube.com/watch?v=" + id, {
+            filter: 'audioonly'
+        });
+        guilds[message.guild.id].skispReq = 0;
+        guilds[message.guild.id].skippers = [];
+
+        guilds[message.guild.id].dispatcher = connection.playStream(stream);
+        guilds[message.guild.id].dispatcher.on('end', function() {
+            guilds[message.guild.id].skipReq = 0;
+            guilds[message.guild.id].skippers = [];
+            guilds[message.guild.id].queue.shift();
+            guilds[message.guild.id].queueNames.shift();
+            if (guilds[message.guild.id].queue.length === 0) {
+                guilds[message.guild.id].queue = [];
+                guilds[message.guild.id].queueNames = [];
+                guilds[message.guild.id].isfPlaying = false;
+            } else {
+                setTimeout(function() {
+                    playMusic(guilds[message.guild.id].queue[0], message);
+                }, 500);
+            }
+        });
+    });
+}
+
+function getID(str, cb) {
+    if (isYoutube(str)) {
+        cb(getYouTubeID(str));
+    } else {
+        search_video(str, function(id) {
+            cb(id);
+        });
+    }
+}
+
+function add_to_queue(strID, message) {
+    if (isYoutube(strID)) {
+        guilds[message.guild.id].queue.push(getYouTubeID(strID));
+    } else {
+        guilds[message.guild.id].queue.push(strID);
+    }
+}
+
+function search_video(query, callback) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + conf.apiToken, function(error, response, body) {
+        var json = JSON.parse(body);
+        if (!json.items[0]) callback("3_-a9nVZYjk");
+        else {
+            callback(json.items[0].id.videoId);
+        }
+    });
+}
+
+function isYoutube(str) {
+    return str.toLowerCase().indexOf("youtube.com") > -1;
+}
+
 /*
 How to commit and update real app:
 0: git status
@@ -627,5 +755,3 @@ How to commit and update real app:
 3. heroku local
 4. git push heroku master
 */
-
-client.login(token);
